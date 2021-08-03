7ea7ffb937c34fc9c0133df1c843397ca97470b5
Rodrigo Marques <marquesfonsecarodrigo@gmail.com>
2021-08-03 10:51:38 -0300

    Atividades cs231n

---
+++ Atividade 1 - Códigos/dl/classifiers/__init__.py
@@ -0,0 +1,2 @@
+from dl.classifiers.k_nearest_neighbor import *
+from dl.classifiers.linear_classifier import *
---
+++ Atividade 1 - Códigos/start_ipython_osx.sh
@@ -0,0 +1,4 @@
+# Assume the virtualenv is called .env
+
+cp config/frameworkpython .env/bin
+.env/bin/frameworkpython -m IPython notebook
